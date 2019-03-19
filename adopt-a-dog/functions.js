8886416ec1e20f40587fcd4214f41bf9b9c023b4
SteveParris74 <steveqube@gmail.com>
2019-03-19 09:22:15 -0700

    trouble

--- adopt-a-dog/functions.js
+++ adopt-a-dog/functions.js
@@ -91,3 +91,72 @@ function runningTotal() {
   total = total + 123.45;
   alert('Your total is $' + total)
 }
+
+const blogPosts = [
+  {
+    headline: 'Blog headline',
+    body: 'Blog content'
+  },
+  {
+    headline: 'Another post',
+    body: 'Second blog content'
+  }
+];
+
+console.log(blogPosts[0].headline);
+
+let blogPost = document.querySelector('article');
+
+console.log(blogPosts);
+
+blogPosts.innerHTML = '<h2>' + blogPosts[0].body + '</p>'
+
+for (let i = 0; i < blogPosts.length; i++) {
+
+}
+
+const blogPosts = [
+{
+  headline: 'Thing about a thing',
+  body: 'some words'
+},
+{
+  headline: 'another blog post',
+  body: 'words have many letters'
+
+}
+];
+
+// blog attempt
+
+cont blogPosts =
+[
+  {
+    headline: 'Traveling with your Dog',
+    body: 'Lorem ipsum etc...',
+    image: '..images/patrick-hendry-221863-unsplash.jpg',
+    alt: 'photo by patrick hendry of dog at grand canyon'
+  }
+];
+let blogPost1 = document.getElementById('blog1');
+
+blogPost1.innerHTML = '<h2 class="Traveling with your Dog">' + blogPosts[0].headline + blogPosts[0].body + blogPosts[0].image + blogPosts[0].alitatuscit
+//not sure where to go from here
+
+//form attempt
+let formElement = document.querySelector('form');
+console.log(formElement);
+
+formElement.addEventListener('submit',function(e){
+  e.preventDefault();
+
+  const inputValue = formElement.elements[0].value;
+  const dogName = formElement.elements[1].value;
+  //do code//
+  const dataToSubmit = {
+  name: inputValue,
+  dog: dogName
+};
+
+  console.log(dataToSubmit);
+})
